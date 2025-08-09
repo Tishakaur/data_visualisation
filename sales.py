10f548f2d3106ea16f3f824d196bb34aefc09599
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2025-08-09 11:38:14 +0530

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Function to read Excel and save chart as PDF
+def chart_visualisation(excel_file, pdf_file):
+    # Read Excel data
+    data = pd.read_excel(r"C:\Users\DELL\Downloads\sales (1).xlsx")
+    
+    # Make bar chart
+    plt.figure(figsize=(6, 4))
+    plt.bar(data['Item'], data['Sales'], color='Blue')
+    plt.xlabel('Item')
+    plt.ylabel('Sales')
+    plt.title('Sales_Analysis')
+    
+    # Save as PDF
+    plt.savefig(pdf_file)
+    plt.close()
+
+# Run the function
+chart_visualisation("sales.xlsx", "sales_chart.pdf")
+
